f01a4073121582b978417e52f709c73a98691f4e
Neven <95269427+Proclus01@users.noreply.github.com>
2022-06-04 23:19:39 -0700

    testing movie API

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-//jshint esversion:6
-const express = require("express");
+//jshint esversion: 7
 
-// Add something in here
+// comment out the line below to make axios work in-browser
+const { default: axios } = require("axios");
 
+// require dotenv to isolate API key in the .env file
+require('dotenv').config();
+
+// Fetch some data using an async function
+// Two arguments are taken: a URI and a {params: {}} object
+const fetchData = async () => {
+
+    // response stores the axios.get request
+    const response = await axios.get('http://www.omdbapi.com/', {
+
+    // params are from the OMDb API documentation: an API key and a 's' search parameter
+        params: {
+            apikey: process.env.API_KEY,
+            s: 'avengers'
+        }
+    });
+
+    // test that we have the data
+    console.log(response.data);
+};
+
+// Run the function
+fetchData();
