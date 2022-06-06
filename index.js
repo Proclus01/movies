402fbdaef35377c8b0233dd1f25f9987a1818d46
Neven <95269427+Proclus01@users.noreply.github.com>
2022-06-06 00:02:42 -0700

    refactor with modules to separate concerns

--- index.js
+++ index.js
@@ -32,29 +32,6 @@ const fetchData = async (searchTerm) => {
 // Select the input box
 const input = document.querySelector('input');
 
-// Debounce outputs a function that serves as a wrapper for onInput  
-// in order to restrict the calls we can make to onInput
-// Debounce take as input a function, and delay in ms with default at 1000
-const debounce = (func, delay = 1000) => {
-    let timeoutId;
-
-    // return a wrapper function to restrict calls to onInput
-    return (...args) => {
-        // clear the current timeoutId and stop the existing timer
-        if (timeoutId) {
-            clearTimeout(timeoutId);
-        }
-
-        // Limit how often the input function gets invoked
-        // It will be called by 'delay' amount after last input
-        timeoutId = setTimeout(() => {
-            // .apply passes in an array of arguments as separate arguments in function
-            func.apply(null, args);
-        }, delay);
-        
-    };
-};
-
 // Callback for event listener
 const onInput = (event) => {
         fetchData(event.target.value);
