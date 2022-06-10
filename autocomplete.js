247e1a7647b0da95ecbc1defbb96aa96a99e0c6b
Neven <95269427+Proclus01@users.noreply.github.com>
2022-06-09 23:45:01 -0700

    moved onInput to autocomplete.js and added config object input to function

--- autocomplete.js
+++ autocomplete.js
@@ -1,4 +1,85 @@
-const createAutoComplete = () => {
-    //
-    
+// input takes config object, starting with root element
+// root element is a reference to the HTML root object in index.html
+const createAutoComplete = ({ root }) => {
+  // Append to 'root' our dropdown menu HTML code
+  root.innerHTML = `
+    <label><b>Search for a Movie</b></label>
+    <input class="input" />
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <div class="dropdown-content results"></div>
+        </div>
+    </div>
+`;
+
+  // Select the input box
+  const input = root.querySelector("input");
+
+  // Selectors for Bulma CSS objects
+  const dropdown = root.querySelector(".dropdown");
+  const resultsWrapper = root.querySelector(".results");
+
+  // Callback for event listener
+  // onInput contains a large part of the main logic of this app
+  // It appends a series of HTML objects to the 'root' dropdown menu, styled with Bulma CSS
+  const onInput = async (event) => {
+    // Get the movie list from OMDb API
+    const movies = await fetchData(event.target.value);
+
+    // If there are no results, return nothing
+    if (!movies.length) {
+      dropdown.classList.remove("is-active");
+      return;
+    }
+
+    // reset the search results first
+    resultsWrapper.innerHTML = "";
+
+    // Turn on the isActive flag inside of our menu item (Bulma CSS)
+    dropdown.classList.add("is-active");
+
+    // Generate a list of HTML objects programmatically from the movies.Search list
+    for (let movie of movies) {
+      const option = document.createElement("a");
+
+      // Check if the poster img has a link, and clear link if val = 'N/A'
+      const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
+
+      // Style the menu using Bulma CSS
+      option.classList.add("dropdown-item");
+
+      // Append HTML to the anchor tag
+      option.innerHTML = `
+            <img src="${imgSrc}" />
+            ${movie.Title} (${movie.Year})
+        `;
+
+      // Add an event listener to the anchor tag for menu functionality
+      option.addEventListener("click", () => {
+        // Make menu disappear on click of anchor
+        dropdown.classList.remove("is-active");
+
+        // Update the text inside the input
+        input.value = movie.Title;
+
+        // Selecting a movie from  the menu
+        onMovieSelect(movie);
+      });
+
+      // Append our looped elements to the results-wrapper (Bulma)
+      resultsWrapper.appendChild(option);
+    }
+  };
+
+  // Attach an event listener to the input box
+  input.addEventListener("input", debounce(onInput, 500));
+
+  // Attach an event listener to test for click events in browser
+  // UI feature: if you click anywhere outside of the menu, the menu will close
+  document.addEventListener("click", (event) => {
+    // If the selected element is not in 'root', then close the menu
+    if (!root.contains(event.target)) {
+      dropdown.classList.remove("is-active");
+    }
+  });
 };
--- index.js
+++ index.js
@@ -35,90 +35,20 @@ const fetchData = async (searchTerm) => {
     return response.data.Search;
 };
 
-// Select the autocomplete HTML tag in index.html
-const root = document.querySelector('.autocomplete');
+// Call createAutocomplete three times to test reusability of autocomplete widget
 
-// Append to 'root' our dropdown menu HTML code 
-root.innerHTML = `
-    <label><b>Search for a Movie</b></label>
-    <input class="input" />
-    <div class="dropdown">
-        <div class="dropdown-menu">
-            <div class="dropdown-content results"></div>
-        </div>
-    </div>
-`;
-
-// Select the input box
-const input = document.querySelector('input');
-
-// Selectors for Bulma CSS objects
-const dropdown = document.querySelector('.dropdown');
-const resultsWrapper = document.querySelector('.results');
-
-// Callback for event listener
-// onInput contains a large part of the main logic of this app
-// It appends a series of HTML objects to the 'root' dropdown menu, styled with Bulma CSS
-const onInput = async (event) => {
-    // Get the movie list from OMDb API
-    const movies = await fetchData(event.target.value);
-
-    // If there are no results, return nothing
-    if (!movies.length) {
-        dropdown.classList.remove('is-active');
-        return;
-    }
-
-    // reset the search results first
-    resultsWrapper.innerHTML = '';
-    
-    // Turn on the isActive flag inside of our menu item (Bulma CSS)
-    dropdown.classList.add('is-active');
-
-    // Generate a list of HTML objects programmatically from the movies.Search list
-    for (let movie of movies) {
-        const option = document.createElement('a');
-
-        // Check if the poster img has a link, and clear link if val = 'N/A'
-        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
-
-        // Style the menu using Bulma CSS
-        option.classList.add('dropdown-item');
-
-        // Append HTML to the anchor tag
-        option.innerHTML = `
-            <img src="${imgSrc}" />
-            ${movie.Title} (${movie.Year})
-        `;
-
-        // Add an event listener to the anchor tag for menu functionality
-        option.addEventListener('click', () => {
-            // Make menu disappear on click of anchor
-            dropdown.classList.remove('is-active');
-
-            // Update the text inside the input
-            input.value = movie.Title;
-
-            // Selecting a movie from  the menu
-            onMovieSelect(movie);
-        });
-
-        // Append our looped elements to the results-wrapper (Bulma)
-        resultsWrapper.appendChild(option);
-    }
-};
-
-// Attach an event listener to the input box
-input.addEventListener('input', debounce(onInput, 500));
-
-// Attach an event listener to test for click events in browser
-// UI feature: if you click anywhere outside of the menu, the menu will close
-document.addEventListener('click', event => {
-    // If the selected element is not in 'root', then close the menu
-    if (!root.contains(event.target)) {
-        dropdown.classList.remove('is-active');
-    }
+createAutoComplete({
+    root: document.querySelector('.autocomplete')
 });
+
+createAutoComplete({
+    root: document.querySelector('.autocomplete-two')
+});
+
+createAutoComplete({
+    root: document.querySelector('.autocomplete-three')
+});
+
 
 // Helper function for onInput.
 // onMovieSelect does a follow up search to fetch the selected movie 
